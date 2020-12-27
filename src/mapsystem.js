bfbc9f76132d073690cb4c1060690a1ea4071f5e
JunxinQiu <55233025+JunxinQiu@users.noreply.github.com>
2020-12-27 19:31:59 +0800

    Add files via upload

---
+++ src/mapsystem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import echarts from 'echarts/lib/echarts';
+import { beijing } from './beijing';
+
+
+
+
+class MapSystem extends React.Component {
+
+    componentDidMount() {
+        echarts.registerMap('beijing', beijing);
+        const chart = echarts.init(document.querySelector('#echarts'));
+        chart.setOption({
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            series: [{
+                type: 'map',
+                map: 'beijing'
+            }]
+        });
+    }
+    render() {
+        return <div>'see what see? not seen before?'</div>;
+    }
+}
+
+ReactDom.render(<MapSystem />, document.querySelector('#root'));
